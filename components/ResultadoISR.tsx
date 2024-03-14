e27c9223ef6c1392f9153a1f3ddd991a349ace67
Sebastián <sebasmoraresu@gmail.com>
2024-03-13 21:04:00 -0500

    :sparkles: feat: improve user experiencie

--- components/ResultadoISR.tsx
+++ components/ResultadoISR.tsx
@@ -9,17 +9,53 @@ const ResultadoISR = () => {
   return (
     <div>
       {ingreso !== 0 && (
-        <>
-          <h1 className="font-bold text-2xl">Resumen</h1>
+        <div className="animate-fade-in">
+          <div className="flex flex-row gap-2 items-center">
+            <h1 className="font-bold text-2xl">Resumen</h1>
+            <div
+              className="tooltip tooltip-right"
+              data-tip="Haz click sobre los números resaltados en negrita para conocer su formula. Esta calculadora solo tiene en cuenta el salario bruto mensual privado y no toma en cuenta otros ingresos o deducciones."
+            >
+              <button
+                type="button"
+                className="btn btn-sm btn-circle hover:animate-pop"
+              >
+                {/* biome-ignore lint/a11y/noSvgWithoutTitle: wip */}
+                <svg
+                  height="16"
+                  strokeLinejoin="round"
+                  viewBox="0 0 16 16"
+                  width="16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM6.25002 7H7.00002H7.75C8.30229 7 8.75 7.44772 8.75 8V11.5V12.25H7.25V11.5V8.5H7.00002H6.25002V7ZM8 6C8.55229 6 9 5.55228 9 5C9 4.44772 8.55229 4 8 4C7.44772 4 7 4.44772 7 5C7 5.55228 7.44772 6 8 6Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
           <p>
             Tu salario bruto mensual es de{" "}
             <span className="font-bold font-mono">${ingreso}</span>
             {calcularISR(ingreso).impuestoAnual > 0 ? (
               <span>
                 , por lo que deberás pagar impuesto sobre la renta del{" "}
-                <span className="font-bold">{tipoDeISR(ingreso * 13)}</span> ya
-                que tu salario anual es de{" "}
-                <span className="font-bold font-mono">${ingreso * 13}</span> (
+                <div
+                  className="tooltip"
+                  data-tip={`$${ingreso} × 13 = $${ingreso * 13} (↑ ${tipoDeISR(
+                    ingreso * 13
+                  )})`}
+                >
+                  <span className="font-bold">{tipoDeISR(ingreso * 13)}</span>
+                </div>{" "}
+                ya que tu salario anual es de{" "}
+                <div className="tooltip" data-tip={`$${ingreso} × 13`}>
+                  <span className="font-bold font-mono">${ingreso * 13}</span>
+                </div>{" "}
+                (
                 <button
                   type="button"
                   className="btn btn-sm btn-ghost underline underline-offset-4"
@@ -32,13 +68,25 @@ const ResultadoISR = () => {
                   para más información, haz click aquí
                 </button>
                 ). Pagarás un total de{" "}
-                <span className="font-bold font-mono">
-                  ${calcularISR(ingreso).impuestoMensual.toFixed(2)}
-                </span>{" "}
+                <div
+                  className="tooltip"
+                  data-tip={`${calcularISR(ingreso).impuestoAnual.toFixed(
+                    2
+                  )} ÷ 13 = ${calcularISR(ingreso).impuestoMensual.toFixed(2)}`}
+                >
+                  <span className="font-bold font-mono">
+                    ${calcularISR(ingreso).impuestoMensual.toFixed(2)}
+                  </span>
+                </div>{" "}
                 al mes, llegando a la suma anual de{" "}
-                <span className="font-bold font-mono">
-                  ${calcularISR(ingreso).impuestoAnual.toFixed(2)}
-                </span>
+                <div
+                  className="tooltip"
+                  data-tip={`Excedente × ${tipoDeISR(ingreso * 13)}`}
+                >
+                  <span className="font-bold font-mono">
+                    ${calcularISR(ingreso).impuestoAnual.toFixed(2)}
+                  </span>
+                </div>
                 .
               </span>
             ) : (
@@ -67,7 +115,7 @@ const ResultadoISR = () => {
               </tbody>
             </table>
           </div>
-        </>
+        </div>
       )}
     </div>
   );
